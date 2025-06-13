bde2aed17cfd0da0f39353f60081026a85a2f46c
Akaza22 <rmrbintang22@gmail.com>
2025-06-14 00:59:26 +0700

    Read service account from environment variables

--- config/firebaseAdmin.js
+++ config/firebaseAdmin.js
@@ -1,11 +1,14 @@
 // config/firebaseAdmin.js
 const admin = require('firebase-admin');
-const serviceAccount = require('../serviceAccountKey.json');
+
+// Cek apakah variabel lingkungan ada (untuk Vercel)
+const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT
+  ? JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)
+  : require('../serviceAccountKey.json'); // Fallback untuk development lokal
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
-
 const db = admin.firestore();
 const auth = admin.auth();
 
